7659613e79f67dedebc6f7a2318e23b9763001ac
Dmitry Anderson <4nd3r5z0n@gmail.com>
2025-06-24 17:49:26 +0200

    Squashed commit

--- modelrunner.py
+++ modelrunner.py
@@ -1,3 +1,4 @@
+import sys
 from modelArh import CarGameAgent
 from gym_env_custom import CustomEnvGAWithQuads
 import time
@@ -14,19 +15,23 @@ stanja = 4
 n_inputs = parametri + stanja + 2 * ray_number
 maxsteps=500
 
-level_file = "levels/10.pkl"
-car_params = (5, 40, 20, [100, 200, 255], 1500, 10, (0, 0, 0), 5)
-
-model_path="models/2/model_rank2_fitness200.00.pth"
-model = CarGameAgent(n_inputs)
-model.load_state_dict(torch.load(model_path))
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-reward=model.run_in_environment(env_fn(), visualize=True, threshold=0.5,maxsteps=200,device=device)
-# best_model.run_in_environment(env_fn(), visualize=True, threshold=0.5,maxsteps=200)
 
 
-end_time = time.time()
+if __name__ == "__main__":
+    pass
+    level_file = "levels/10.pkl"
+    car_params = (5, 40, 20, [100, 200, 255], 1500, 10, (0, 0, 0), 5)
 
-print(f"Execution time: {end_time - start_time:.6f} seconds")
+    model_path=sys.argv[1]
+    model = CarGameAgent(n_inputs)
+    model.load_state_dict(torch.load(model_path))
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    reward=model.run_in_environment(env_fn(), visualize=True, threshold=0.5,maxsteps=200,device=device)
+    # best_model.run_in_environment(env_fn(), visualize=True, threshold=0.5,maxsteps=200)
+
+
+    end_time = time.time()
+
+    print(f"Execution time: {end_time - start_time:.6f} seconds")
